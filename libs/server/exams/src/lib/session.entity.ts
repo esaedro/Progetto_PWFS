8ec01ec3aa42cf01f28032274c1d34db47482f61
LucaViola17 <130687039+LucaViola17@users.noreply.github.com>
2026-05-05 15:26:55 +0200

    Eliminazione classe "Secretariat"

--- libs/server/exams/src/lib/session.entity.ts
+++ libs/server/exams/src/lib/session.entity.ts
@@ -1,7 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Exam } from './exam.entity';
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { Secretariat } from '@server/people';
+
 
 @Entity('Sessions')
 export class Session {
@@ -23,7 +22,4 @@ export class Session {
   @OneToMany(() => Exam, (exam) => exam.session)
   exams: Exam[];
 
-  @ManyToOne(() => Secretariat, (secretariat) => secretariat.sessions)
-  @JoinColumn({ name: 'secretariat_id' })
-  secretary: Secretariat;
 }
--- libs/server/people/src/index.ts
+++ libs/server/people/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/people.controller';
 export * from './lib/people.service';
 export * from './lib/people.module';
-export * from './lib/secretariat.entity';
 export * from './lib/professor.entity';
