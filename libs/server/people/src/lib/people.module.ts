d69a50bb6095223b3787aa8eb5e457af71032f17
LucaViola17 <130687039+LucaViola17@users.noreply.github.com>
2026-05-08 16:18:23 +0200

    import people module

--- libs/server/people/src/lib/people.module.ts
+++ libs/server/people/src/lib/people.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServerPeopleController } from './people.controller';
 import { ServerPeopleService } from './people.service';
+import { Professor } from './professor.entity';
+import { PeopleRepository } from './people.repository';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Professor])],
   controllers: [ServerPeopleController],
-  providers: [ServerPeopleService],
+  providers: [ServerPeopleService, PeopleRepository],  //repository nei provider? 
   exports: [ServerPeopleService],
 })
 export class ServerPeopleModule {}
